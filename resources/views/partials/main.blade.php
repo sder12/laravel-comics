ac0805a5a060d1e57c36e252b52b478ecb58aebc
sder12 <sara.cetto@gmail.com>
2022-12-15 16:10:17 +0100

    Refactoring cards

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,41 +1,44 @@
-
-{{-- Banner --}}
+<main>
+    {{-- Banner --}}
     <div class="banner">
         <span class="d-none">banner</span>
     </div>
 
-{{-- CARDS --}}
+    {{-- CARDS --}}
     <div class="container-main">
         <div class="wrapper">
             <section class="cards-container">
-               {{-- TITLE --}}
-                <div class="cards-title">
-                    <h4>current series</h4>
-                </div>
-                
-                {{-- CARDS 6x2 --}}   
-                <div class="cards-wrapper">       
-                        @foreach ($data as $card)
-                      
-                        <div class="card-wrapper">
-                            <div class="card-img">
-                                <img src="{{$card['thumb']}}" alt="{{$card['title']}}>
-                                <span class="card-price"> {{ $card['price'] }}</span>
+
+                {{-- TITLE --}}
+                    <div class="cards-title">
+                        <h4>current series</h4>
+                    </div>
+                    
+                    {{-- CARDS 6x2 --}}   
+                    <div class="cards-wrapper">       
+                            @foreach ($data as $card)
+                        
+                            <div class="card-wrapper">
+                                <div class="card-img">
+                                    <img src="{{$card['thumb']}}" alt="{{$card['title']}}">
+                                </div>
+                                <div class="card-subtitle pt-2">
+                                    <span>{{ $card['series'] }}</span>
+                                </div>
                             </div>
-                            <div class="card-subtitle">
-                                <h4>{{ $card['title'] }}</h4>
-                            </div>
-                        </div>
-                            
-                        @endforeach
-                    </div >
-                
-                {{-- LOAD MORE --}}
-                <div class="cards-button">
-                    <button>
-                        load more
-                    </button>
-                </div>
+                                
+                            @endforeach
+                        </div >
+                    
+                    {{-- LOAD MORE --}}
+                    <div class="cards-button">
+                        <button>
+                            load more
+                        </button>
+                    </div>
+
             </section>
         </div>
     </div>
+
+</main>
